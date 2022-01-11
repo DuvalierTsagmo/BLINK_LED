dd2ee5ff2f1a6ebeba1bd46d37f95691f5208af5
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-01-11 10:02:43 -0400

    deuxieme version

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,30 @@
 
 /*
- Titre : Titre du programme
+ Titre : Blink LED
  Auteur : Tsagmo Duvalier
- Date : 30/10/2021
- Description : Une description du programme
+ Date : 11/01/2022
+ Description : faire clignoter une LED
  Version : 0.0.1
-*/ 
-
+*/
 
 #include <Arduino.h>
 
-const int ledPin = 2;  //led sur broche digitale 2
+const int ledPin = 2; //led sur broche digitale 2
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);  //mode sortie pour broche
+  pinMode(ledPin, OUTPUT); //mode sortie pour broche
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
   //boucle continue pour allumer et eteindre la broche
 
-  digitalWrite(ledPin,HIGH);   //broche place en mode HAUT (3.3V)
+  digitalWrite(ledPin, HIGH); //broche place en mode HAUT (3.3V)
   delay(500);                 //delai
-  digitalWrite(ledPin,LOW);    //broche place en mode BAS (0V)
+  digitalWrite(ledPin, LOW);  //broche place en mode BAS (0V)
   delay(500);                 //delai
 }
